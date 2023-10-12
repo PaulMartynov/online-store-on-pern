820f35dbb152da6de4532768facea8a551a3b408
PaulMartynov <juanfen@yandex.ru>
2023-10-12 17:17:22 +0300

    add database connection

--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,26 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
+
+import sequelize from "./db";
 
 const app: Express = express();
 const port = process.env.SERVER_PORT || 5000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello!");
+  res.send("Hello1");
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+const start = async () => {
+  try {    
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(port, () => {
+      console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
