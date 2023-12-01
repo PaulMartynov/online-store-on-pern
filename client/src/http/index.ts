9085906d55d87befa725edeca218091cc3a3eb19
PaulMartynov <juanfen@yandex.ru>
2023-12-01 15:14:58 +0300

    fix http config

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,18 +1,16 @@
 import axios from "axios";
 
 const $host = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.BACKEND_ADDR}:${process.env.BACKEND_PORT}`,
 });
 
 const $authHost = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
+  baseURL: `${process.env.BACKEND_ADDR}:${process.env.BACKEND_PORT}`,
 });
 
-const authInterceptor = (config: any) => {
+$authHost.interceptors.request.use((config) => {
   config.headers.authorization = `Bearer ${localStorage.getItem("token")}`;
   return config;
-};
-
-$authHost.interceptors.request.use(authInterceptor);
+});
 
 export { $host, $authHost };
